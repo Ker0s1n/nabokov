d9567a5a933db0dc9776dbe1fbcfab5ca802a5a9
HomePCwsl <chachimin@yandex.ru>
2025-02-05 23:01:39 +0300

    try this

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-# Exit on error
-set -o errexit
-
-uv sync
--- run-docker.sh
+++
@@ -1,5 +0,0 @@
-sudo groupadd docker
-sudo usermod -aG docker $USER
-newgrp docker # start docker permissions without reboot
-
-sudo service docker restart # start docker without reboot
